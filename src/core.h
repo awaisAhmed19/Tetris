05b352b5ea4888b48ff997132fe72ad582f13a10
awaisAhmed19 <awaisahmedoffi@gmail.com>
2026-08-16 23:52:31 +0530

    adding time tick and piece controll

--- src/core.h
+++ src/core.h
@@ -3,6 +3,7 @@
 typedef uint8_t u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
+typedef uint64_t u64;
 
 typedef int8_t i8;
 typedef int16_t i16;
@@ -17,17 +18,27 @@ typedef int32_t i32;
 #define BOARD_HEIGHT 20
 #define BOARD_WIDTH 10
 
-#define HOLD_W 150
-#define HOLD_H 150
+#define HOLD_W (CELL_W * 4)
+#define HOLD_H (CELL_W * 4)
 
-#define HOLD_X 440
-#define HOLD_Y 50
+#define BOARD_X ((SCREENWIDTH - (BOARD_WIDTH * CELL_W)) * 0.5)
+#define BOARD_Y ((SCREENHEIGHT - (BOARD_HEIGHT * CELL_H)) * 0.5)
+
+#define HOLD_X (BOARD_X + (BOARD_WIDTH * CELL_H) + 10)
+#define HOLD_Y ((SCREENHEIGHT - (BOARD_HEIGHT * CELL_H)) * 0.5)
 
 // #define SPAWN_X 5
 // #define SPAWN_Y 0
 
-const i16 paddingW = 150;
-const i16 paddingH = 50;
+typedef struct {
+  const i16 x;
+  const i16 y;
+
+  i16 w;
+  i16 h;
+
+  i8 state[BOARD_WIDTH][BOARD_HEIGHT];
+} game_board;
 
 typedef struct {
   u8 r;
--- src/main.c
+++ src/main.c
@@ -6,7 +6,9 @@
 #include <stdlib.h>
 #include <tetrominos.h>
 #include <time.h>
+const Uint64 FALL_INTERVAL = 900;
 int board[BOARD_HEIGHT][BOARD_WIDTH];
+
 void draw_rect(SDL_Renderer *rend, SDL_FRect *rect, Color c) {
   SDL_SetRenderDrawColor(rend, c.r, c.g, c.b, c.a);
   SDL_RenderFillRect(rend, rect);
@@ -25,8 +27,8 @@ void draw_board(SDL_Renderer *rend) {
   for (int i = 0; i < BOARD_HEIGHT; ++i) {
     for (int j = 0; j < BOARD_WIDTH; ++j) {
       c = (board[i][j]) ? Piece_colors[board[i][j] - 1] : base_color;
-      SDL_FRect rect = {.x = CELL_W * j + paddingW,
-                        .y = CELL_H * i + paddingH,
+      SDL_FRect rect = {.x = CELL_W * j + BOARD_X,
+                        .y = CELL_H * i + BOARD_Y,
                         .w = CELL_W,
                         .h = CELL_H};
       draw_rect(rend, &rect, c);
@@ -34,17 +36,17 @@ void draw_board(SDL_Renderer *rend) {
   }
 }
 
-void set_piece(SDL_Renderer *rend, Piece p) {
-  switch (p.type) {
+void draw_piece(SDL_Renderer *rend, Piece *p) {
+  switch (p->type) {
   case TT_I:
     for (int i = 0; i < 4; i++) {
-      int x = p.cell.x + (p.rotation) ? I_rot1[i].x : I_rot0[i].x;
-      int y = p.cell.y + (p.rotation) ? I_rot1[i].y : I_rot0[i].y;
-      SDL_FRect rect = {.x = CELL_W * x + paddingW,
-                        .y = CELL_H * y + paddingH,
+      int x = p->cell.x + (p->rotation ? I_rot1[i].x : I_rot0[i].x);
+      int y = p->cell.y + (p->rotation ? I_rot1[i].y : I_rot0[i].y);
+      SDL_FRect rect = {.x = CELL_W * x + BOARD_X,
+                        .y = CELL_H * y + BOARD_Y,
                         .w = CELL_W,
                         .h = CELL_H};
-      draw_rect(rend, &rect, Piece_colors[p.type - 1]);
+      draw_rect(rend, &rect, Piece_colors[p->type - 1]);
     }
     break;
   case TT_O:
@@ -69,6 +71,41 @@ void draw_hold_window(SDL_Renderer *rend) {
   SDL_SetRenderDrawColor(rend, base_color.r, base_color.g, base_color.b,
                          base_color.a);
   SDL_RenderFillRect(rend, &rect);
+}
+
+bool check_bounds(Piece *p) {
+  switch (p->type) {
+  case TT_I:
+    return p->cell.x >= 0 && p->cell.x <= BOARD_WIDTH;
+    break;
+  case TT_O:
+    break;
+  case TT_T:
+    break;
+  case TT_S:
+    break;
+  case TT_Z:
+    break;
+  case TT_J:
+    break;
+  case TT_L:
+    break;
+  }
+  return false;
+}
+bool check_piece_valid(Piece *c) { return true; }
+void update(Piece *p) {
+
+  Piece candidate = *p;
+  candidate.cell.y++;
+  *p = candidate;
+  // if (check_piece_valid(&candidate)) {
+  //   *p = candidate;
+  // } else {
+  //   // eventually:
+  //   // lock_piece(p);
+  //   // spawn_piece(p);
+  // }
 }
 int main() {
   srand((unsigned)time(0));
@@ -96,8 +133,9 @@ int main() {
   };
 
   init_board();
-  // set_piece(board, p);
+  // draw_piece(board, p);
   bool running = true;
+  u64 last_fall = SDL_GetTicks();
   while (running) {
     SDL_Event e;
     while (SDL_PollEvent(&e)) {
@@ -107,10 +145,22 @@ int main() {
         break;
       case SDL_EVENT_KEY_DOWN:
         if (e.key.key == SDLK_A || e.key.key == SDLK_LEFT) {
-          p.cell.x--;
+          if (p.cell.x <= 0) {
+            p.cell.x = 0;
+          } else {
+            p.cell.x--;
+          }
+          printf("P->x--: %d\n", p.cell.x);
         }
         if (e.key.key == SDLK_D || e.key.key == SDLK_RIGHT) {
-          p.cell.x++;
+          if (p.cell.x >= BOARD_WIDTH - 1 ||
+              (p.rotation == 0 && p.cell.x >= BOARD_WIDTH - 4)) {
+            p.cell.x = p.rotation ? BOARD_WIDTH - 1 : BOARD_WIDTH - 4;
+
+          } else {
+            p.cell.x++;
+          }
+          printf("P->x++: %d\n", p.cell.x);
         }
         if (e.key.key == SDLK_SPACE) {
           p.rotation = !(p.rotation);
@@ -119,12 +169,18 @@ int main() {
         break;
       }
     }
+    u64 now = SDL_GetTicks();
+    if (now - last_fall > FALL_INTERVAL) {
+      update(&p);
+      last_fall = now;
+    }
     SDL_SetRenderDrawColor(rend, black.r, black.g, black.b, black.a);
     SDL_RenderClear(rend);
     draw_board(rend);
     draw_hold_window(rend);
-    set_piece(rend, p);
+    draw_piece(rend, &p);
     SDL_RenderPresent(rend);
+    // SDL_Delay(1500);
   }
   SDL_DestroyRenderer(rend);
   SDL_DestroyWindow(win);
